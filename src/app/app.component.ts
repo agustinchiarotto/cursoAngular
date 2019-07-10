e16170cfefec03daff4672c7159785694fd8b1ac
agustinchiarotto <agusgc_99@hotmail.com>
2019-07-10 11:37:46 -0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CursoAngular';
+  nombre:string = "Agus";
+  apellido:string ="Chiarotto"
+}
