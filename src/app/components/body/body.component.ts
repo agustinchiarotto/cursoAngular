9b8206bc6990949988a8fbe85a61f974892755f6
Agus <agusgc_99@hotmail.com>
2019-07-14 14:44:28 -0300

    segunda app

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
 })
 
 export class BodyComponent{
+    mostrar:boolean=true;
+    frase:any = {
+        cita:"Hola Mundo",
+        autor:"Agus Chiarotto",
+    };
+
+    personajes:string[]= [
+            'Agus','David','Anatasia'
+    ]
+    
 
 }
